b4c19ba86bcf57af75fec7ea6c8cb93ddccd700e
Maia Bell <maiarose04@gmail.com>
2022-11-08 10:34:03 -0800

    finishes input activity javascript

--- script/script.js
+++ script/script.js
@@ -1,3 +1,23 @@
-alert
-    ("enter something") 
-    console.log("this is really fun")
+// alert
+//     ("enter something") 
+//     console.log("this is really fun")
+
+
+// for (let i = 2; i <= 1024; i = i * 2){
+//     console.log(i)
+//   }
+
+// let i = 1;
+// while (i < 1024){
+//     i = i * 2;
+//     console.log(i);
+//   }
+
+let input = ""
+
+do{
+    input = prompt("What is your name?: ")}
+    while(Number(input) || input == 0);
+
+alert(`Welcome ${input}`);
+
