1c694c2caac5dcf62f38db674c2581f75debe20a
Maia Bell <maiarose04@gmail.com>
2022-11-08 10:55:17 -0800

    tries printing javascript acitvities

--- script/script.js
+++ script/script.js
@@ -13,11 +13,39 @@
 //     console.log(i);
 //   }
 
-let input = ""
+// let input = ""
 
-do{
-    input = prompt("What is your name?: ")}
-    while(Number(input) || input == 0);
+// do{
+//     input = prompt("What is your name?: ")}
+//     while(Number(input) || input == 0);
 
-alert(`Welcome ${input}`);
+// alert(`Welcome ${input}`);
+
+// let fruits = ['Apple', 'Banana'];
+
+// fruits.push("Oranges");
+// console.log(fruits);
+
+// fruits.shift();
+// console.log(fruits);
+
+// fruits.unshift("Mango");
+// console.log(fruits);
+
+// fruits.pop();
+// console.log(fruits);
+
+const person ={
+    name: ['bob', 'smith'],
+    age: 32,
+    gender: 'male',
+    interests: ['music', 'skiing'],
+}
+
+console.log(`My name is ${person.name[0]} ${person.name[1]} i am interested in ${person.interests[0]} and ${person.interests[1]}`)
+
+
+
+
+
 
